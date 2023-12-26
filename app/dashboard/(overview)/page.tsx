d0794f48e122b009ef625740ac3b613e181bb17a
Amin <aminkhalif99@gmail.com>
2023-12-26 16:16:00 -0500

    card wrapper and card streaming
    - added card wrapper to make skeleton load with all cards in congruence.
    
    -added card streaming for parallel rendering

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,4 +1,5 @@
-import { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper from '@/app/ui/dashboard/cards';
+
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
@@ -7,16 +8,11 @@ import { Suspense } from 'react';
 import {
   RevenueChartSkeleton,
   LatestInvoicesSkeleton,
+  CardsSkeleton ,
 } from '@/app/ui/skeletons';
 
 export default async function Page() {
 
-  const {
-    numberOfInvoices,
-    numberOfCustomers,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
 
   return (
     <main>
@@ -24,9 +20,9 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {<Card title="Collected" value={totalPaidInvoices} type="collected" />}
-        {<Card title="Pending" value={totalPendingInvoices} type="pending" />}
-        {<Card title="Total Invoices" value={totalPendingInvoices} type="invoices" />}
+      <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
         {/* <Card
           title="Total Customers"
           value={numberOfCustomers}
