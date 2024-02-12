f2c4dc07983897b31320ffd01fa2dac29169ea47
Amin <aminkhalif99@gmail.com>
2024-02-11 21:37:43 -0500

    Added Pagination
    Added pagination . This tells what page we are on in the URL  (application is broken temporary)

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -5,13 +5,13 @@ import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
-  const pathname = usePathname();
   const { replace } = useRouter();
-
+  const pathname = usePathname();
+  
   const handleSearch = useDebouncedCallback((term) => {
-    console.log(`Searching... ${term}`);
-
+   // console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1')
     if (term) {
       params.set('query', term);
     } else {
